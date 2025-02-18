d2ee5446750370553b71128ea5d51c568ca4f182
jeel1607 <jeelpatidar1607@gmail.com>
2025-02-18 21:24:26 +0530

    my 3rd issue

--- src/COLLECTION/Demo.java
+++ src/COLLECTION/Demo.java
@@ -5,5 +5,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("dkfdfm");
         System.out.println("mfe");
+        System.out.println("sdsdcd");
+        System.out.println("dksns");
     }
 }
