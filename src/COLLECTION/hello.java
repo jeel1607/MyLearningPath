e90ccdf8425a80a58aa465c84393718e711db56b
jeel1607 <jeelpatidar1607@gmail.com>
2025-02-19 12:48:28 +0530

    add new filer

--- src/COLLECTION/hello.java
+++ src/COLLECTION/hello.java
@@ -3,5 +3,7 @@ package COLLECTION;
 public class hello {
     public static void main(String[] args) {
         System.out.println("fmdfdkfb");
+        System.out.println("hello mkc");
+        System.out.println("dndfefefefgit ");
     }
 }
